7ef8d044d57b9b0626ce0f4af9c4133911b4d419
Thomas Fanell <thomas.fanell@imint.se>
2015-08-06 13:45:59 +0200

    improved EmptyLines rule

--- source/analyzer/rules/EmptyLinesRule.ts
+++ source/analyzer/rules/EmptyLinesRule.ts
@@ -11,38 +11,59 @@ class EmptyLinesRule implements Rule {
 	run(tokens: Array<Token>, report: Report) {
 		var linefeeds = 0;
 		var linefeedLocation: TokenLocation;
-		var previous = Token.empty;
+		var foundLeftCurly = false;
 		for (var i = 0; i < tokens.length; i++) {
-			if (tokens[i].kind == TokenKind.WhitespaceLineFeed) {
-				var enough = false;
-				linefeedLocation = tokens[i + 1].location;
-				while (!enough) {
-					i++;
-					switch (tokens[i].kind) {
-						case TokenKind.WhitespaceLineFeed:
-							linefeeds++;
-							break;
-						case TokenKind.WhitespaceSpace:
-						case TokenKind.WhitespaceTab:
-							break;
-						default:
-							enough = true;
-							break;
+			switch (tokens[i].kind) {
+				case TokenKind.WhitespaceLineFeed:
+					var enough = false;
+					linefeedLocation = tokens[i + 1].location;
+					while (!enough) {
+						i++;
+						switch (tokens[i].kind) {
+							case TokenKind.WhitespaceLineFeed:
+								linefeeds++;
+								break;
+							case TokenKind.WhitespaceSpace:
+							case TokenKind.WhitespaceTab:
+								break;
+							case TokenKind.SeparatorRightCurly:
+								foundLeftCurly = false;
+							default:
+								enough = true;
+								break;
+						}
 					}
-				}
+					break;
+				case TokenKind.SeparatorLeftCurly:
+					foundLeftCurly = true;
+					break;
+				case TokenKind.WhitespaceSpace:
+				case TokenKind.WhitespaceTab:
+				case TokenKind.LineComment:
+				case TokenKind.BlockComment:
+					break;
+				default:
+					foundLeftCurly = false;
+					break;
 			}
 			if (linefeeds > 0 && tokens[i].kind == TokenKind.Eof) {
 				report.addViolation(new Violation(linefeedLocation,
 					"unnecessary empty line(s) before end of file", RuleKind.Whitespace));
-			} else if (linefeeds > 0 && tokens[i].kind == TokenKind.SeparatorRightCurly) {
+			}
+			if (linefeeds > 0 && foundLeftCurly) {
+				report.addViolation(new Violation(linefeedLocation,
+					"unnecessary empty line(s) after opening curly", RuleKind.Whitespace));
+				foundLeftCurly = false;
+			}
+			if (linefeeds > 0 && tokens[i].kind == TokenKind.SeparatorRightCurly) {
 				report.addViolation(new Violation(linefeedLocation,
 					"unnecessary empty line(s) before closing curly", RuleKind.Whitespace));
-			}else if (linefeeds > 1) {
+			}
+			if (linefeeds > 1) {
 				report.addViolation(new Violation(linefeedLocation,
 					"too many empty lines: " + linefeeds, RuleKind.Whitespace));
 			}
 			linefeeds = 0;
-			previous = tokens[i];
 		}
 	}
 }
