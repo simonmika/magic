91ab9d044f72ee01f8bae87d133f94e21b07ccb3
Thomas Fanell <thomas.fanell@imint.se>
2015-09-01 21:50:11 +0200

    updated Lexer test

--- source/Tokens/Tests/LexerTest.ts
+++ source/Tokens/Tests/LexerTest.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../Error/ConsoleHandler" />
 /// <reference path="../../IO/StringReader" />
 /// <reference path="../../Unit/Fixture" />
 /// <reference path="../../Unit/Constraints/Is" />
@@ -9,7 +10,19 @@ module Magic.Tokens.Tests {
 	export class LexerTest extends Unit.Fixture {
 		constructor() {
 			super("LexerTest")
-			
+			var errorHandler = new Error.ConsoleHandler()
+			this.add("expression 1", () => {
+				var lexer = new Lexer(new IO.StringReader("a+b\0"), errorHandler)
+				this.expect(lexer.next(), Is.Not().NullOrUndefined())
+				this.expect(lexer.next(), Is.Not().NullOrUndefined())
+				this.expect(lexer.next(), Is.Not().NullOrUndefined())
+			})
+			this.add("expression 2", () => {
+				var lexer = new Lexer(new IO.StringReader("a+b\0"), errorHandler)
+				this.expect(lexer.next().isIdentifier())
+				this.expect(lexer.next().isOperator())
+				this.expect(lexer.next().isIdentifier())
+			})
 		}
 	}
 }
