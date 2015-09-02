551c4835325006b14103f47fbe790bab4350f5d2
Thomas Fanell <thomas.fanell@imint.se>
2015-09-02 11:22:29 +0200

    added StringReaderTest

---
+++ source/IO/Tests/StringReaderTest.ts
@@ -0,0 +1,75 @@
+/// <reference path="../../Error/ConsoleHandler" />
+/// <reference path="../../Error/Position" />
+/// <reference path="../../Error/Location" />
+/// <reference path="../../Error/Region" />
+/// <reference path="../../IO/StringReader" />
+/// <reference path="../../Unit/Fixture" />
+/// <reference path="../../Unit/Constraints/Is" />
+
+module Magic.IO.Tests {
+	import Is = Unit.Constraints.Is
+	export class StringReaderTest extends Unit.Fixture {
+		constructor() {
+			super("StringReader")
+			var errorHandler = new Error.ConsoleHandler()
+			this.add("empty", () => {
+				var sr = new IO.StringReader("")
+				this.expect(sr.isEmpty())
+				this.expect(sr.getResource() === "")
+			})
+			this.add("simple string", () => {
+				var sr = new IO.StringReader("abcdef")
+				this.expect(sr.read() === "a")
+				this.expect(sr.read() === "b")
+				this.expect(sr.read() === "c")
+				this.expect(sr.read() === "d")
+				this.expect(sr.read() === "e")
+				this.expect(sr.read() === "f")
+			})
+			this.add("simple string with location", () => {
+				var sr = new IO.StringReader("abc\ndef")
+				this.expect(sr.getLocation().getColumn() === 1 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 2 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 3 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 4 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 1 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 2 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 3 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.isEmpty())
+			})
+			this.add("tabs and newlines", () => {
+				var sr = new IO.StringReader("\t\t\t\n\t\t\t")
+				this.expect(sr.getLocation().getColumn() === 1 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 2 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 3 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 4 && sr.getLocation().getLine() === 1)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 1 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 2 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.getLocation().getColumn() === 3 && sr.getLocation().getLine() === 2)
+				sr.read()
+				this.expect(sr.isEmpty())
+			})
+			this.add("mark", () => {
+				var sr = new IO.StringReader("abc\0")
+				sr.mark()
+				sr.read(); sr.read(); sr.read()
+				var region = sr.getRegion()
+				this.expect(region.getStart().getLine() === 1 && region.getStart().getColumn() === 1)
+				this.expect(region.getEnd().getLine() === 1 && region.getEnd().getColumn() === 4)
+			})
+		}
+	}
+}
--- source/SelfTest.ts
+++ source/SelfTest.ts
@@ -5,20 +5,23 @@
 /// <reference path="./Unit/Tests/UndefinedTest" />
 /// <reference path="./Unit/Tests/EqualTest" />
 /// <reference path="./Unit/Tests/NotTest" />
+/// <reference path="./IO/Tests/StringReaderTest" />
 /// <reference path="./Tokens/Tests/LexerTest" />
 
 module Magic {
 	export class SelfTest {
 		static run() {
 			var tests: Unit.Fixture[] = [
-				// Unit/Tests
+				// Unit framework tests
 				new Unit.Tests.BooleanTest(),
 				new Unit.Tests.NullTest(),
 				new Unit.Tests.NullOrUndefinedTest(),
 				new Unit.Tests.UndefinedTest(),
 				new Unit.Tests.EqualTest(),
 				new Unit.Tests.NotTest(),
-				// Tokens/Tests
+				// IO tests
+				new IO.Tests.StringReaderTest(),
+				// Tokens tests
 				new Tokens.Tests.LexerTest()
 			]
 			tests.forEach(test => {
