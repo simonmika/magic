26c1c7a0513860d816fe5af4613cef7f6ffe20af
Thomas Fanell <thomas.fanell@imint.se>
2015-09-04 12:36:05 +0200

    added IdentifierTest

---
+++ source/Tokens/Tests/IdentifierTest.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../Error/ConsoleHandler" />
+/// <reference path="../../IO/StringReader" />
+/// <reference path="../../Unit/Fixture" />
+/// <reference path="../../Unit/Constraints/Is" />
+/// <reference path="../Token" />
+/// <reference path="../Identifier" />
+/// <reference path="../Source" />
+
+module Magic.Tokens.Tests {
+	import Is = Unit.Constraints.Is
+	export class IdentifierTest extends Unit.Fixture {
+		constructor() {
+			super("Tokens.Identifier")
+			var errorHandler = new Error.ConsoleHandler()
+			this.add("isIdentifier()", () => {
+				var identifier1 = new Identifier(null, null)
+				var identifier2 = new Identifier("bar", null)
+				this.expect(identifier1.isIdentifier())
+				this.expect(identifier1.isIdentifier("foo"), Is.False())
+				this.expect(identifier2.isIdentifier())
+				this.expect(identifier2.isIdentifier("foo"), Is.False())
+			})
+			this.add("scan identifier", () => {
+				var source = new Source(new IO.StringReader("identifier"), errorHandler)
+				var identifier = Identifier.scan(source)
+				this.expect(identifier instanceof Identifier)
+				this.expect(identifier.isIdentifier())
+				this.expect((<Identifier>identifier).getName(), Is.Equal().To("identifier"))
+			})
+		}
+	}
+	Unit.Fixture.add(new IdentifierTest())
+}
