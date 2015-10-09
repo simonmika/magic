d3e69f33360abb02a590a62f9d1ed7e928b5833a
Thomas Fanell <thomas.fanell@vidhance.com>
2015-10-09 16:56:05 +0200

    Fixed grammar

--- source/SyntaxTree/Type/Function.ts
+++ source/SyntaxTree/Type/Function.ts
@@ -2,11 +2,11 @@
 
 module Magic.SyntaxTree.Type {
 	export class Function extends Expression {
-		constructor(private argument: Expression[], private result: Expression, tokens: Tokens.Substance[]) {
+		constructor(private argumentList: Expression[], private result: Expression, tokens: Tokens.Substance[]) {
 			super(tokens)
 		}
-		getArgument(): Expression[] {
-			return this.argument
+		getArguments(): Expression[] {
+			return this.argumentList
 		}
 		getResult(): Expression {
 			return this.result
