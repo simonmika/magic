5c144019db507534c27667a042cad15024639487
Thomas Fanell <thomas.fanell@vidhance.com>
2015-10-13 15:58:55 +0200

    started on expanding Function declaration

--- source/SyntaxTree/Declarations/Function.ts
+++ source/SyntaxTree/Declarations/Function.ts
@@ -10,8 +10,6 @@
 /// <reference path="Argument" />
 /// <reference path="../Block" />
 
-
-
 module Magic.SyntaxTree.Declarations {
 	export class Function extends Declaration {
 		constructor(symbol: Type.Name, private typeParameters: Type.Name[], private argumentList: Argument[], private returnType: Type.Expression, private body: Block, tokens: Tokens.Substance[]) {
@@ -20,19 +18,29 @@ module Magic.SyntaxTree.Declarations {
 		getTypeParameters(): Type.Name[] {
 			return this.typeParameters
 		}
+		getReturnType(): Type.Expression {
+			return this.returnType
+		}
+		getBody(): Block {
+			return this.body
+		}
 		static parse(source: Source): Function {
 			var result: Function
 			// TODO: add support for modifiers: override, virtual, abstract, static
-			if (source.peek(0).isIdentifier() && source.peek(1).isSeparator(":") && source.peek(2).isIdentifier("func")) {
+			if (source.peek(0).isIdentifier() && source.peek(1).isSeparator(":") && (source.peek(2).isIdentifier("func") || source.peek(3).isIdentifier("func"))) {
 				var symbol = Type.Name.parse(source.clone())
 				source.next() // consume ":"
+				if (!source.peek().isIdentifier("func"))
+					source.next() // TODO: <-- this is the modifier
 				source.next() // consume "func"
 				// TODO: add overload name parsing: ~overloadName
 				var typeParameters = Declaration.parseTypeParameters(source)
 				var argumentList = Argument.parseAll(source)
 				var returnType: Type.Expression
-				if (source.peek().isSeparator(":"))
+				if (source.peek().isOperator("->")) {
+					source.next() // consume "->"
 					returnType = Type.Expression.parse(source)
+				}
 				var body = Block.parse(source)
 				result = new Function(symbol, typeParameters, argumentList, returnType, body, source.mark())
 			}
