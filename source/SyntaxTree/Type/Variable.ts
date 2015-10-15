4e731de02065f781351940db900a84ccfacb1ffa
Thomas Fanell <thomas.fanell@vidhance.com>
2015-10-15 12:40:44 +0200

    Removed Variable from Type namespace

--- source/SyntaxTree/Type/Variable.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="Expression" />
-/// <reference path="../Source" />
-
-
-module Magic.SyntaxTree.Type {
-	export class Variable extends Expression {
-		// TODO: Implement class
-	}
-}
