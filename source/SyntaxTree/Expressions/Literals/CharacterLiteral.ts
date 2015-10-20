83766153ab98d19ed45122df581ea3bc2da44f55
Thomas Fanell <thomas.fanell@vidhance.com>
2015-10-20 13:28:21 +0200

    Added CharacterLiteral

---
+++ source/SyntaxTree/Expressions/Literals/CharacterLiteral.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../../Tokens/Substance" />
+/// <reference path="../../../Tokens/Literals/Char" />
+/// <reference path="../../Source" />
+/// <reference path="../Expression" />
+
+module Magic.SyntaxTree.Expressions {
+	export class CharacterLiteral extends Expression {
+		constructor(private value: string, tokens: Tokens.Substance[]) {
+			super(tokens)
+		}
+		static parse(source: Source): CharacterLiteral {
+			var result: CharacterLiteral
+			if (source.peek().isLiteral())
+				result = new CharacterLiteral((<Tokens.Literals.Char>source.next()).getValue(), source.mark())
+			return result
+		}
+	}
+	Expression.addParser(CharacterLiteral.parse)
+}
